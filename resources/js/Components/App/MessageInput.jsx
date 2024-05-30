0c3521a15061f6accf249699fdc809e3b8e82b0c
Donat <tasidonat@gmail.com>
2024-05-30 22:19:44 +0200

    Audio recording function

--- resources/js/Components/App/MessageInput.jsx
+++ resources/js/Components/App/MessageInput.jsx
@@ -7,6 +7,7 @@ import { Popover, Transition } from "@headlessui/react";
 import { isAudio, isImage } from "@/helpers";
 import CustomAudioPlayer from "./CustomAudioPlayer";
 import AttachmentPreview from "./AttachmentPreview";
+import AudioRecorder from "./AudioRecorder";
 
 const MessageInput = ({ conversation = null }) => {
     const [newMessage, setNewMessage] = useState("");
@@ -94,6 +95,10 @@ const MessageInput = ({ conversation = null }) => {
 
         axios.post(route("message.store"), data);
     };
+
+    const recorderAudioReady = (file, url) => {
+        setChosenFiles((prevFiles) => [...prevFiles, {file, url}]);
+    }
     
     return (
         <div className="flex flex-wrap items-start border-t border-slate-700 py-3">
@@ -117,6 +122,7 @@ const MessageInput = ({ conversation = null }) => {
                         className="absolute left-0 top-0 right-0 bottom-0 z-20 opacity-0 cursor-pointer"
                     />
                 </button>
+                <AudioRecorder fileReady={recorderAudioReady} />
             </div>
             <div className="order-1 px-3 xs:p-0 min-w-[220px] basis-full xs:basis-0 xs:order-2 flex-1 relative">
                 <div className="flex">
