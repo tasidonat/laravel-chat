7e9147998517773a93de36938a84b70f47990e98
Donat <tasidonat@gmail.com>
2024-06-05 21:16:47 +0200

    Fix: issues with deleting last message

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -128,6 +128,8 @@ class MessageController extends Controller
 
         $message->delete();
 
+        $lastMessage = null;
+
         if($group) {
             $group = Group::find($group->id);
             $lastMessage = $group->lastMessage;
